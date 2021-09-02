b4edbe2766de7f165b76c53627bf9136bef22212
il <fadoilulmun.im2807@gmail.com.com>
2021-09-02 17:33:55 +0700

    no expired token

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -41,7 +41,7 @@ class GoogleController extends Controller
                 return response()->json([
                     'access_token' => $token,
                     'token_type' => 'bearer',
-                    'expires_in' => auth()->factory()->getTTL() * 60,
+                    'expires_in' => 'no expired',
                     'user' => auth()->user()
                 ]);;
     
